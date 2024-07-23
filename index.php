4d19c9e23a9a4dfb0e5ca9405a8b9dd9b530ef77
Vicky Greig <vickygreig@hotmail.co.uk>
2024-07-23 14:40:02 +0100

    finishing artists home display

--- index.php
+++ index.php
@@ -13,8 +13,8 @@ $artists = $artistsModel->getAllArtists();
 $displayArtists = $artistsModel->displayThreeArtists($artists);
 
 
-
-echo '<!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -30,8 +30,8 @@ echo '<!DOCTYPE html>
             <div class="grid md:grid-cols-2 lg:grid-cols-3 mt-12 gap-5">
                 <div class="">
                     <h3 class="text-xl font-bold mb-3">Artists</h3>
-                    <div class="grid grid-cols-2 gap-3 mb-3">';
-                    foreach ($displayArtists as $artist) {
+                    <div class="grid grid-cols-2 gap-3 mb-3">
+                    <?php foreach ($displayArtists as $artist) {
                         echo "
                         <a class='rounded bg-cyan-950 p-3 hover:bg-cyan-800 hover:cursor-pointer'>
                             <div class='flex gap-2 h-8'>
@@ -41,19 +41,19 @@ echo '<!DOCTYPE html>
                             <h4 class='text-xl font-bold'>{$artist->getArtistName()}</h4>
                             <p>{$artist->getAlbumCount()} Albums</p>
                             </a>";
-                    }
-                       echo '<div class="rounded bg-cyan-950 p-3 flex items-center">
+                    } ?>
+                        <div class="rounded bg-cyan-950 p-3 flex items-center">
                             <h4 class="text-2xl text-slate-500">+ 15 more</h4>
                         </div>
                     </div>
-                    <a href="artists.html" class="float-right border rounded-md bg-cyan-950 px-2 py-1 hover:bg-cyan-800">See all
+                    <a href="Html/sde-music-player-template/artists.html" class="float-right border rounded-md bg-cyan-950 px-2 py-1 hover:bg-cyan-800">See all
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 inline">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                         </svg>
                     </a>
                 </div>
 </body>
-</html>';
+</html>
 
 
 
