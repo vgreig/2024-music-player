7a705f9ce70bb203356cbe4671a3ad1e4cc2b6f3
Max Estrella <maxestrella565@gmail.com>
2024-07-23 10:37:05 +0100

    Foreach loop Songs

--- index.php
+++ index.php
@@ -31,6 +31,8 @@ echo '<pre>';
 var_dump($songsInAlbum);
 echo '</pre>';
 
+echo $album->getAlbumName();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -57,41 +59,44 @@ echo '</pre>';
                 </a>
             </div>
             <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-5 items-start">
-                <?php
-                foreach ($displayAlbums as $album) {
-                    echo "<h4 class='mb-3 text-2xl font-bold'>$album</h4>";
-                }
-
+<!--                --><?php
+//                foreach ($displayAlbums as $album) {
+//                    echo "<h4 class='mb-3 text-2xl font-bold'>$album</h4>";
+//                }
+//
                 ?>
                 <div class="rounded p-3 bg-cyan-950">
                     <!--ALBUM NAME-->
 
-                    <h4 class="mb-3 text-2xl font-bold"><?php echo $songsInAlbum[0]->getSongName(); ?></h4>
-                    <div class="mx-3 mb-3 flex justify-between items-center">
-                        <div class="w-3/4 pe-3">
+                    <h4 class="mb-3 text-2xl font-bold"><?php echo $album->getAlbumName(); ?></h4>
+                    <?php
+                    foreach ($songsInAlbum as $songInAlbum) {
+                        echo "
+                    <div class='mx-3 mb-3 flex justify-between items-center'>
+                        <div class='w-3/4 pe-3'>
                             <!--SONG NAME-->
-
-                            <h4 class="font-bold text-lg"><?php echo $songsInAlbum[0]->getSongName(); ?></h4>
+                            <h4 class='font-bold text-lg'>{$songInAlbum->getSongName()}</h4>
                             <!--PLAY NUMBER-->
-                            <p class="text-sm">Played <?php echo $songsInAlbum[0]->getPlayCount(); ?>  times</p>
+                            <p class='text-sm'>Played {$songInAlbum->getPlayCount()} times</p>
                         </div>
-                        <div class="flex items-center justify-between w-24">
+                        <div class='flex items-center justify-between w-24'>
                             <!--LENGTH-->
-                            <span class="text-slate-500"><?php echo $songsInAlbum[0]->getLength(); ?></span>
-
-                            <a href="?playSong=1" class="hover:text-slate-500 hover:cursor-pointer">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 inline">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z"></path>
+                            <span class='text-slate-500'>{$songInAlbum->getLength()}</span>
+                                        
+                            <a href='?playSong=1' class='hover:text-slate-500 hover:cursor-pointer'>
+                                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='size-6 inline'>
+                                    <path stroke-linecap='round' stroke-linejoin='round' d='M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'></path>
+                                    <path stroke-linecap='round' stroke-linejoin='round' d='M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z'></path>
                                 </svg>
                             </a>
-                            <a href="favourite.php?id=" class="hover:text-slate-500 hover:cursor-pointer">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+                            <a href='favourite.php?id=' class='hover:text-slate-500 hover:cursor-pointer'>
+                                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='size-6'>
+                                    <path stroke-linecap='round' stroke-linejoin='round' d='M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z' />
                                 </svg>
                             </a>
                         </div>
-                    </div>
+                    </div>";}
+                    ?>
                 </div>
 
             </div>
