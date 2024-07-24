7c74c6b7d56804687a067ecff8fa2ef1612e93d4
Max Estrella <maxestrella565@gmail.com>
2024-07-24 17:01:53 +0100

    UpdatePlayCount displaying in DisplayArtistsAlbums

--- src/Services/DisplayArtistsAlbums.php
+++ src/Services/DisplayArtistsAlbums.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+require_once 'src/Services/PlayCountService.php';
+
 class DisplayArtistsAlbums {
     public static function displayAlbums(array $artistsAlbums, SongsModel $songs): string
     {
@@ -21,8 +23,7 @@ class DisplayArtistsAlbums {
                         </div>
                         <div class='flex items-center justify-between w-24'>
                             <span class='text-slate-500'>{$songInAlbum->getLength()}</span>";
-                $albums .=  $playButton = UpdatePlayCount::playCount(4);
-                            echo $playButton;
+                $albums .=  UpdatePlayCount::playCount(4);
                 $albums .= "<a href='favourite.php?id=' class='hover:text-slate-500 hover:cursor-pointer'>
                                 <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='size-6'>
                                     <path stroke-linecap='round' stroke-linejoin='round' d='M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z' />
