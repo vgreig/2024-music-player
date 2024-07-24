797658bf29bc03bbb0388cadb9dc55b70e072769
Max Estrella <maxestrella565@gmail.com>
2024-07-24 15:16:54 +0100

    index setup for testing

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
 
+require_once 'src/Models/SongsModel.php';
+require_once 'src/Entities/Song.php';
+require_once 'src/Services/PlayCountService.php';
+require_once 'src/Services/DatabaseConnector.php';
+
+$db = DatabaseConnector::connect();
+
+$songId = 1;
+
+$playButton = new SongsModel($db);
+$buttonTest = $playButton->getSongsByAlbum($songId);
+
+$playButton = UpdatePlayCount::playCount($buttonTest, $songId);
+echo $playButton;
