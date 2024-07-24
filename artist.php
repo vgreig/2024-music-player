846b1d2d0a545bc6e99b4e7ed182dbfd990f81e7
Will Atkinson <willreubena@gmail.com>
2024-07-24 14:28:44 +0100

    changed service name

--- artist.php
+++ artist.php
@@ -65,7 +65,7 @@ $artistsAlbums = $albums->getAlbumsByArtistId($artistId);
         <section class="group-[.minimised]:h-[calc(100%-6rem)] h-3/4 p-12 overflow-auto">
             <div class="flex justify-between">
                 <h2 class="text-4xl font-bold mb-6"><?php echo $artist->getArtistName(); ?></h2>
-                <a href="artists.html" class="align-top">
+                <a href="artists.php" class="align-top">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 inline">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
                     </svg>
